31f1217c2421faee6fc1c1f603bb124768ef8d4f
Łukasz Łakomy <l.lakomy@webcon.pl>
2021-10-10 23:56:38 +0200

    add test template

---
+++ src/utils/DesireSwap/supply.test.ts
@@ -0,0 +1,96 @@
+import { BigNumber } from 'ethers';
+import { token0Supply, token1Supply } from 'utils/DesireSwap/supply';
+
+const prettyPrintObj = (obj: Record<string, string | number | BigNumber>) =>
+  Object.entries(obj)
+    .map(([key, val]) => `\n\t${key}: ${val?.toString()}`)
+    .join('');
+
+fdescribe('#DesireSwap - supply', () => {
+  describe('returns correct amount and liquidity for token0Supply', () => {
+    const testCases = [
+      {
+        funcArguments: {
+          amount: BigNumber.from(0),
+          lowestRangeIndex: 0,
+          highestRangeIndex: 0,
+          inUseRange: 0,
+          ticksInRange: 0,
+          reserve0: BigNumber.from(0),
+          reserve1: BigNumber.from(0),
+          liquidity: BigNumber.from(0),
+        },
+        expectedValues: { liquidity: BigNumber.from(0), amount: BigNumber.from(0) },
+      },
+      {
+        funcArguments: {
+          amount: BigNumber.from(0),
+          lowestRangeIndex: 0,
+          highestRangeIndex: 0,
+          inUseRange: 0,
+          ticksInRange: 0,
+          reserve0: BigNumber.from(0),
+          reserve1: BigNumber.from(0),
+          liquidity: BigNumber.from(0),
+        },
+        expectedValues: { liquidity: BigNumber.from(0), amount: BigNumber.from(0) },
+      },
+    ];
+    testCases.forEach((testCase) =>
+      it(`\nargs: ${prettyPrintObj(testCase.funcArguments)}\n expected: ${prettyPrintObj(
+        testCase.expectedValues
+      )}`, () => {
+        expect(false).toBeTruthy();
+        const { liqToAdd, amount } = token0Supply(
+          testCase.funcArguments.amount,
+          testCase.funcArguments.lowestRangeIndex,
+          testCase.funcArguments.highestRangeIndex,
+          testCase.funcArguments.inUseRange,
+          testCase.funcArguments.ticksInRange,
+          testCase.funcArguments.reserve0,
+          testCase.funcArguments.reserve1,
+          testCase.funcArguments.liquidity
+        );
+        expect(liqToAdd.eq(testCase.expectedValues.liquidity)).toBeTruthy();
+        expect(amount.eq(testCase.expectedValues.amount)).toBeTruthy();
+      })
+    );
+  });
+
+  describe('returns correct amount and liquidity for token1Supply', () => {
+    const testCases = [
+      {
+        funcArguments: {
+          amount: BigNumber.from(0),
+          lowestRangeIndex: 0,
+          highestRangeIndex: 0,
+          inUseRange: 0,
+          ticksInRange: 0,
+          reserve0: BigNumber.from(0),
+          reserve1: BigNumber.from(0),
+          liquidity: BigNumber.from(0),
+        },
+        expectedValues: { liquidity: BigNumber.from(0), amount: BigNumber.from(0) },
+      },
+    ];
+    testCases.forEach((testCase) =>
+      it(`\nargs: ${prettyPrintObj(testCase.funcArguments)}\n expected: ${prettyPrintObj(
+        testCase.expectedValues
+      )}`, () => {
+        expect(false).toBeTruthy();
+        const { liqToAdd, amount } = token1Supply(
+          testCase.funcArguments.amount,
+          testCase.funcArguments.lowestRangeIndex,
+          testCase.funcArguments.highestRangeIndex,
+          testCase.funcArguments.inUseRange,
+          testCase.funcArguments.ticksInRange,
+          testCase.funcArguments.reserve0,
+          testCase.funcArguments.reserve1,
+          testCase.funcArguments.liquidity
+        );
+        expect(liqToAdd.eq(testCase.expectedValues.liquidity)).toBeTruthy();
+        expect(amount.eq(testCase.expectedValues.amount)).toBeTruthy();
+      })
+    );
+  });
+});
